4d1c5954a56cc21bafbaaeb6b33b521e5b484c41
Fabian Sefranek <fabiansefranek@gmail.com>
2024-10-23 15:29:03 +0200

    Add perf tests

--- go.mod
+++ go.mod
@@ -28,9 +28,11 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.6.1 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
 	github.com/moby/sys/sequential v0.5.0 // indirect
@@ -43,6 +45,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"log"
+	"math/rand"
+	"os"
 	"time"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/mongodb"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -15,7 +18,6 @@ import (
 )
 
 func main() {
-	/* POSTGRES */
 	postgresConnectionString, postgresContainer, err := StartPostgres()
 	if err != nil {
 		panic(err)
@@ -28,49 +30,168 @@ func main() {
 	}
 	defer postgresConn.Close(context.Background())
 
-	var result int
-
-	err = postgresConn.QueryRow(context.Background(), "SELECT 1").Scan(&result)
-	if err != nil {
-		panic(err)
-	}
-
 	err = InitializePostgres(postgresConn)
 	if err != nil {
 		panic(err)
 	}
 
-	/* MONGODB */
-
-	mongodbConnectionString, mongodbContainer, err := StartMongoDB()
+	mongoConnectionString, mongoContainer, err := StartMongoDB()
 	if err != nil {
 		panic(err)
 	}
 	defer func() {
-		if err := testcontainers.TerminateContainer(mongodbContainer); err != nil {
+		if err := testcontainers.TerminateContainer(mongoContainer); err != nil {
 			log.Printf("failed to terminate container")
 			panic(err)
 			
 		}
 	}()
 
-	mongodbConn, err := ConnectMongoDB(mongodbConnectionString)
+	mongoConn, err := ConnectMongoDB(mongoConnectionString)
 	if err != nil {
 		panic(err)
 	}
 	defer func() {
-		if err := mongodbConn.Disconnect(context.Background()); err != nil {
+		if err := mongoConn.Disconnect(context.Background()); err != nil {
 			panic(err)
 		}
 	}()
 
-	err = InitializeMongoDB(mongodbConn)
+	err = InitializeMongoDB(mongoConn)
 	if err != nil {
 		panic(err)
 	}
 
+	projects := GenerateProjects(10000)
+	tableRows := make([][]string, 0)
+
+	/* PERFORMANCE TESTS */
+
+	/* insert */
+
+	postgresDuration, err := InsertPostgres(postgresConn, projects)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err := InsertMongo(mongoConn, projects)
+	if err != nil {
+		panic(err)
+	}
+
+	tableRows = append(tableRows, []string{"Insert", postgresDuration, mongoDuration})
+
+	/* find */
+
+	postgresDuration, err = FindPostgres(postgresConn)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err = FindMongo(mongoConn)
+	if err != nil {
+		panic(err)
+	}
+
+	tableRows = append(tableRows, []string{"Find", postgresDuration, mongoDuration})
+
+	postgresDuration, err = FindPostgresWithFilter(postgresConn)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err = FindMongoWithFilter(mongoConn)
+	if err != nil {
+		panic(err)
+	}
+
+	tableRows = append(tableRows, []string{"Find with filter", postgresDuration, mongoDuration})
+
+	postgresDuration, err = FindPostgresWithFilterAndProjection(postgresConn)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err = FindMongoWithFilterAndProjection(mongoConn)
+	if err != nil {
+		panic(err)
+	}
+
+	tableRows = append(tableRows, []string{"Find with filter and projection", postgresDuration, mongoDuration})
+
+	postgresDuration, err = FindPostgresWithFilterAndProjectionAndSort(postgresConn)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err = FindMongoWithFilterAndProjectionAndSort(mongoConn)
+	if err != nil {
+		panic(err)
+	}
+
+	tableRows = append(tableRows, []string{"Find with filter and projection and sort", postgresDuration, mongoDuration})
+
+	/* update */
+
+	postgresDuration, err = UpdatePostgres(postgresConn)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err = UpdateMongo(mongoConn)
+	if err != nil {
+		panic(err)
+	}
+
+	tableRows = append(tableRows, []string{"Update", postgresDuration, mongoDuration})
+
+	/* delete */
+
+	postgresDuration, err = DeletePostgres(postgresConn)
+	if err != nil {
+		panic(err)
+	}
+
+	mongoDuration, err = DeleteMongo(mongoConn)
+	if err != nil {
+		panic(err)
+	}
+	
+	tableRows = append(tableRows, []string{"Delete", postgresDuration, mongoDuration})
+
+	/* TABLE */
+
+	PrintTable(tableRows)
+
 	time.Sleep(1 * time.Hour) 
 }
+
+/* TYPES */
+
+type Project struct {
+	Id int
+	Name string
+	Identifier string
+	InviteCode string
+	SprintDuration int
+	Owner User
+	Sprints []Sprint
+}
+
+type User struct {
+	Id int
+	Username string
+	FirstName string
+	LastName string
+}
+
+type Sprint struct {
+	Id int
+	Name string
+	StartDate int64
+	EndDate int64
+}
+
 
 /* POSTGRES */
 
@@ -111,9 +232,8 @@ func InitializePostgres(conn *pgx.Conn) (err error) {
 			`CREATE TABLE users (
 				id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 				username VARCHAR(255) NOT NULL,
-				password VARCHAR(255) NOT NULL,
 				first_name VARCHAR(255) NOT NULL,
-				last_name VARCHAR(255) NOT NULL
+				last_name VARCHAR(255	) NOT NULL
 			);
 
 			CREATE TABLE projects (
@@ -130,8 +250,8 @@ func InitializePostgres(conn *pgx.Conn) (err error) {
 				id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 				name VARCHAR(255) NOT NULL,
 				project_id INT NOT NULL,
-				start_date DATE NOT NULL,
-				end_date DATE NOT NULL,
+				start_date INT NOT NULL,
+				end_date INT NOT NULL,
 				CONSTRAINT fk_project FOREIGN KEY(project_id) REFERENCES projects(id)
 			);
 `)
@@ -176,4 +296,311 @@ func InitializeMongoDB(client *mongo.Client) (err error) {
 	}
 
 	return nil
+}
+
+/* SEEDER */
+
+func RandomString(length int) string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandomTimestamp() int64 {
+	return rand.Int63n(time.Now().Unix() - 94608000) + 94608000
+}
+
+/* func GenerateStrings(arraySize int, stringLength int) []string {
+	var result []string
+	for i := 0; i < arraySize; i++ {
+		result = append(result, RandomString(stringLength))
+	}
+
+	return result
+}
+
+func GenerateTimestamps(arraySize int) []string {
+	var result []string
+	for i := 0; i < arraySize; i++ {
+		result = append(result, RandomTimestamp())
+	}
+
+	return result
+} */
+
+func GenerateProjects(arraySize int) []Project {
+	var result []Project
+	for i := 0; i < arraySize; i++ {
+		result = append(result, Project{
+			Name: RandomString(10),
+			Identifier: RandomString(48),
+			InviteCode: RandomString(128),
+			SprintDuration: rand.Intn(100) + 1,
+			Owner: User{
+				Username: RandomString(10),
+				FirstName: RandomString(10),
+				LastName: RandomString(10),
+			},
+			Sprints: []Sprint{
+				{
+					Name: RandomString(10),
+					StartDate: RandomTimestamp(),
+					EndDate: RandomTimestamp(),
+				},
+			},
+		})
+	}
+	return result
+}
+
+/* PERFORMANCE TESTS */
+
+func InsertPostgres(conn *pgx.Conn, projects []Project) (duration string, err error) {
+	now := time.Now()
+	for _, project := range projects {
+		var userId int
+		err = conn.QueryRow(context.Background(), 
+			`INSERT INTO users (username, first_name, last_name) VALUES ($1, $2, $3) RETURNING id;`,
+			project.Owner.Username, project.Owner.FirstName, project.Owner.LastName).Scan(&userId)
+		if err != nil {
+			return "", err
+		}
+
+		var projectId int
+		err = conn.QueryRow(context.Background(),
+			`INSERT INTO projects (name, identifier, invite_code, sprint_duration, owner_id) VALUES ($1, $2, $3, $4, $5) RETURNING id;`,
+			project.Name, project.Identifier, project.InviteCode, project.SprintDuration, userId).Scan(&projectId)
+		if err != nil {
+			return "", err
+		}
+
+		for _, sprint := range project.Sprints {
+			var sprintId int
+			err = conn.QueryRow(context.Background(),
+				`INSERT INTO sprints (name, project_id, start_date, end_date) VALUES ($1, $2, $3, $4) RETURNING id;`,
+				sprint.Name, projectId, sprint.StartDate, sprint.EndDate).Scan(&sprintId)
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+
+	return time.Since(now).String(), nil
+}
+
+func FindPostgres(conn *pgx.Conn) (duration string, err error) {
+	now := time.Now()	
+	_, err = conn.Exec(context.Background(), `SELECT * FROM sprints INNER JOIN projects ON sprints.project_id = projects.id INNER JOIN users ON projects.owner_id = users.id;`)
+	if err != nil {
+		return "", err
+	}
+
+	return time.Since(now).String(), nil
+}
+
+func FindPostgresWithFilter(conn *pgx.Conn) (duration string, err error) {
+	now := time.Now()	
+	_, err = conn.Exec(context.Background(), `SELECT * FROM sprints INNER JOIN projects ON sprints.project_id = projects.id INNER JOIN users ON projects.owner_id = users.id WHERE projects.sprint_duration > 50;`)
+	if err != nil {
+		return "", err
+	}
+
+	return time.Since(now).String(), nil
+}
+
+func FindPostgresWithFilterAndProjection(conn *pgx.Conn) (duration string, err error) {
+	now := time.Now()
+	_, err = conn.Exec(context.Background(), `SELECT users.username, projects.name, sprints.name FROM sprints INNER JOIN projects ON sprints.project_id = projects.id INNER JOIN users ON projects.owner_id = users.id WHERE projects.sprint_duration > 50;`)
+	if err != nil {
+		return "", err
+	}
+
+	return time.Since(now).String(), nil
+}
+
+func FindPostgresWithFilterAndProjectionAndSort(conn *pgx.Conn) (duration string, err error) {
+	now := time.Now()
+	_, err = conn.Exec(context.Background(), `SELECT users.username, projects.name, sprints.name FROM sprints INNER JOIN projects ON sprints.project_id = projects.id INNER JOIN users ON projects.owner_id = users.id WHERE projects.sprint_duration > 50 ORDER BY sprints.start_date DESC;`)
+	if err != nil {
+		return "", err
+	}
+
+	return time.Since(now).String(), nil
+}
+
+func UpdatePostgres(conn *pgx.Conn) (duration string, err error) {
+	now := time.Now()
+	_, err = conn.Exec(context.Background(), `UPDATE sprints SET start_date = start_date + (60*60*24)`)
+	if err != nil {
+		return "", err
+	}
+	return time.Since(now).String(), nil
+}
+
+func DeletePostgres(conn *pgx.Conn) (duration string, err error) {
+	now := time.Now()
+	_, err = conn.Exec(context.Background(), `DELETE FROM sprints`)
+	if err != nil {
+		return "", err
+	}
+	return time.Since(now).String(), nil
+}
+
+func InsertMongo(client *mongo.Client, projects []Project) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	for _, project := range projects {
+		_, err = client.Database("test").Collection("projects").InsertOne(ctx, bson.M{
+			"name": project.Name,
+			"identifier": project.Identifier,
+			"invite_code": project.InviteCode,
+			"sprint_duration": project.SprintDuration,
+			"owner": bson.M{
+				"username": project.Owner.Username,
+				"first_name": project.Owner.FirstName,
+				"last_name": project.Owner.LastName,
+			},
+			"sprints": bson.A{
+				bson.M{
+					"name": project.Sprints[0].Name,
+					"start_date": project.Sprints[0].StartDate,
+					"end_date": project.Sprints[0].EndDate,
+				},
+			},
+		})
+		if err != nil {
+			return "", err
+		}
+	}
+	return time.Since(now).String(), nil
+}
+
+func FindMongo(client *mongo.Client) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	cursor, err := client.Database("test").Collection("projects").Find(ctx, bson.M{})
+	if err != nil {
+		return "", err
+	}
+	cursor.Close(ctx)
+
+	return time.Since(now).String(), nil
+}
+
+func FindMongoWithFilter(client *mongo.Client) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	cursor, err := client.Database("test").Collection("projects").Find(ctx, bson.M{"sprint_duration": bson.M{"$gt": 50}})
+	if err != nil {
+		return "", err
+	}
+	cursor.Close(ctx)
+
+	return time.Since(now).String(), nil
+}
+
+func FindMongoWithFilterAndProjection(client *mongo.Client) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	cursor, err := client.Database("test").Collection("projects").Find(
+		ctx,
+		bson.M{
+			"sprint_duration": bson.M{"$gt": 50},
+		},
+		options.Find().SetProjection(bson.M{
+			"name": 1,
+			"identifier": 1,
+			"invite_code": 1,
+			"owner": bson.M{
+				"username": 1,
+				"first_name": 1,
+				"last_name": 1,
+			},
+			"sprints": bson.M{
+				"name": 1,
+				"start_date": 1,
+				"end_date": 1,
+			},
+		}),
+	)
+	if err != nil {
+		return "", err
+	}
+	cursor.Close(ctx)
+
+	return time.Since(now).String(), nil
+}
+
+func FindMongoWithFilterAndProjectionAndSort(client *mongo.Client) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	cursor, err := client.Database("test").Collection("projects").Find(
+		ctx,
+		bson.M{
+			"sprint_duration": bson.M{"$gt": 50},
+		},
+		options.Find().SetProjection(bson.M{
+			"name": 1,
+			"identifier": 1,
+			"invite_code": 1,
+			"owner": bson.M{
+				"username": 1,
+				"first_name": 1,
+				"last_name": 1,
+			},
+			"sprints": bson.M{
+				"name": 1,
+				"start_date": 1,
+				"end_date": 1,
+			},
+		}).SetSort(bson.M{
+			"sprints.start_date": -1,
+		}),
+	)
+	if err != nil {
+		return "", err
+	}
+	cursor.Close(ctx)
+
+	return time.Since(now).String(), nil
+}
+
+func UpdateMongo(client *mongo.Client) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	_, err = client.Database("test").Collection("projects").UpdateMany(ctx, bson.M{}, bson.M{"$inc": bson.M{"sprint_duration": (60*60*24)}})
+	if err != nil {
+		return "", err
+	}
+
+	return time.Since(now).String(), nil
+}
+
+func DeleteMongo(client *mongo.Client) (duration string, err error) {
+	now := time.Now()
+	ctx := context.Background()
+	_, err = client.Database("test").Collection("projects").DeleteMany(
+		ctx,
+		bson.M{},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return time.Since(now).String(), nil
+}
+
+/* TABLE */
+
+func PrintTable(rows [][]string) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"#", "Query", "Postgres", "Mongo"})
+	for _, row := range rows {
+		t.AppendRow(table.Row{1000, row[0], row[1], row[2]})
+	}
+	t.Render()
 }
